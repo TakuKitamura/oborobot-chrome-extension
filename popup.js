59495e7c586003c630e099280dcba28ee22df90b
TakuKitamura <tikutaku0128@icloud.com>
2019-12-26 13:35:43 +0900

    http -> https

--- popup.js
+++ popup.js
@@ -5,49 +5,56 @@
 // changeColor.onclick = function (element) {
 //     // let color = element.target.value;
 chrome.tabs.query({
-    active: true,
-    currentWindow: true
-}, function (tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id, {
+        active: true,
+        currentWindow: true
+    },
+    function (tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {
             // code: 'document.body.style.backgroundColor = "' + color + '";'
-            file: 'sendMessage.js'
+            file: "sendMessage.js"
         });
-});
+    }
+);
 
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        // console.log(sender.tab ?
-        //     "from a content script:" + sender.tab.url :
-        //     "from the extension");
-        // alert(3)
-        if (request.type === 'favorite') {
-            // alert(1)
-            const xhr = new XMLHttpRequest();
-            xhr.open("POST", 'http://localhost:3000/api/user/favorite', true);
-            xhr.send(JSON.stringify({
-                href: sender.tab.url,
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    // console.log(sender.tab ?
+    //     "from a content script:" + sender.tab.url :
+    //     "from the extension");
+    // alert(3)
+    if (request.type === "favorite") {
+        // alert(1)
+        const xhr = new XMLHttpRequest();
+        xhr.open(
+            "POST",
+            "https://oborobot.asuscomm.com:1072/api/user/favorite",
+            true
+        );
+        xhr.send(
+            JSON.stringify({
+                href: sender.tab.url
                 // lang: request.lang,
-            }))
-            let favoriteValue = document.getElementById('favorite-value');
-            favoriteValue.innerText = '記事を登録しました｡'
-        } else if (request.type === 'googleSearch') {
-            // alert(2)
-            let searchBoxValue = document.getElementById('question-value');
-            searchBoxValue.innerText = 'もっと検索する｡'
+            })
+        );
+        let favoriteValue = document.getElementById("favorite-value");
+        favoriteValue.innerText = "記事を登録しました｡";
+    } else if (request.type === "googleSearch") {
+        // alert(2)
+        let searchBoxValue = document.getElementById("question-value");
+        searchBoxValue.innerText = "もっと検索する｡";
 
-            const xhr = new XMLHttpRequest();
-            // xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.open("POST", 'http://localhost:3000/api/user/query', true);
-            xhr.send(JSON.stringify({
-                href: sender.tab.url,
+        const xhr = new XMLHttpRequest();
+        // xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.open("POST", "https://oborobot.asuscomm.com:1072/api/user/query", true);
+        xhr.send(
+            JSON.stringify({
+                href: sender.tab.url
                 // lang: request.lang,
                 // searchValue: request.searchBoxValue
-            }))
+            })
+        );
 
-            sendResponse({
-                status: "ok"
-            });
-        }
-
-    });
+        sendResponse({
+            status: "ok"
+        });
+    }
+});
